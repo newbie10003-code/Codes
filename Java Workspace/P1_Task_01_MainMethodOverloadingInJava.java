16e4ae5c636f9e19ce9db96057a006d555520a03
newbie10003-code <newbie10003-code@github.com>
2023-04-08 00:15:23 +0530

    Changes

--- Java Workspace/P1_Task_01_MainMethodOverloadingInJava.java
+++ Java Workspace/P1_Task_01_MainMethodOverloadingInJava.java
@@ -5,12 +5,22 @@ public class P1_Task_01_MainMethodOverloadingInJava {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello Uma !!!");
-
+		Test a = new Test();
+		a.add(5, 3);
+		main();
 	}
 	static void main()
 	{
 		System.out.println("Hello ....this is overloaded main method ");
-	
+		Test a = new Test();
+		a.add(5, 3);
 	}
+}
 
+class Test
+{
+	void add(int a, int b)
+	{
+		System.out.println(a + b);
+	}
 }

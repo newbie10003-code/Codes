b2bb9b309032c1920b836a545a6eca9089e5a874
newbie10003-code <117057897+newbie10003-code@users.noreply.github.com>
2023-06-16 18:02:38 +0530

    Changes

---
+++ Web Dev/Demo/requests.js
@@ -0,0 +1,27 @@
+const http = require("http")
+const data = JSON.stringify({
+    title : "MEAN Stack"
+})
+
+const options = {
+    hostname : "localhost",
+    port : 3050,
+    path : '/',
+    method : "POST",
+    headers : {
+        'Content-type' : "application/json",
+        "Content-Length" : data.length
+    }
+}
+
+const request = http.request(options, response => {
+    response.on('data', chunk => {
+        process.stdout.write(chunk);
+    })
+})
+
+request.on('error', error => {
+    console.error("Erro")
+})
+
+request.end()

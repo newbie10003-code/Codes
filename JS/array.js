c5a9b9de797bbc024368f02734ac167e496e8aa8
Nilesh Bhanot <newbie10003-code@github.com>
2024-03-02 12:04:42 +0530

    Changes

---
+++ JS/array.js
@@ -0,0 +1,37 @@
+const myArr = [1, 2, 3, 4, "Newbie"]
+const myHeroes = ["Superman", "Batman", "Avengers", "Spiderman", "Ironman"]
+
+const myArr2 = new Array(myHeroes)
+// console.log(myArr[4]);
+// console.log(myArr2[4]);
+// console.log(myHeroes[3]);
+
+// Array methods
+
+// myArr.push("Wonderwoman")
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.indexOf(4));
+// console.log(myArr.includes("vfhb"));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+// console.log(newArr[4]);
+
+// console.log("A ", myArr);
+// const newArr = myArr.slice(1, 3)
+// console.log(newArr);
+// console.log("B ", myArr);
+
+
+console.log("A ", myArr);
+const newArr = myArr.splice(1, 3)
+console.log(newArr);
+console.log("B ", myArr);

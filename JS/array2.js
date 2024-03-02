ef9ac360a96109e7a364db2210a8bad64307f18f
Nilesh Bhanot <newbie10003-code@github.com>
2024-03-02 19:45:54 +0530

    Array methods 2

--- JS/array2.js
+++ JS/array2.js
@@ -1,7 +1,27 @@
 const marvelHeroes = ["Thor", 'Ironman', 'Spiderman']
 const dcHeroes = ["Batman", "Superman", "Flash"]
 
-marvelHeroes.push(dcHeroes)
+// marvelHeroes.push(dcHeroes)
 
-console.log(marvelHeroes);
-console.log(marvelHeroes[3][1]);
+// console.log(marvelHeroes);
+// console.log(marvelHeroes[3][1]);
+
+// newHeroes = marvelHeroes.concat(dcHeroes)
+// console.log(newHeroes);
+
+// newHeroes = [...marvelHeroes, ...dcHeroes]
+// console.log(newHeroes);
+
+// anotherArr = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// realArr = anotherArr.flat(Infinity)
+// console.log(realArr);
+
+// console.log(Array.isArray("Newbie"))
+// console.log(Array.from("Newbie"))
+// console.log(Array.from({Name : "Newbie"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
